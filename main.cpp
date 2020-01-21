48bfe63a0832a0625a7a6a7ea1083d5866b132d7
Owen L - SFE <owen.lyke@sparkfun.com>
2020-01-21 09:40:23 -0700

    remove mbed cpp constructs

--- main.cpp
+++ main.cpp
@@ -7,13 +7,23 @@
 
 #define WAIT_TIME 500 //msec
 
-DigitalOut led1(LED1);
+// DigitalOut led1(LED1);
 
 int main()
 {
+    // Set the clock frequency.
+    am_hal_clkgen_control(AM_HAL_CLKGEN_CONTROL_SYSCLK_MAX, 0);
+
+    // Set the default cache configuration
+    am_hal_cachectrl_config(&am_hal_cachectrl_defaults);
+    am_hal_cachectrl_enable();
+
+    // Configure the board for low power operation.
+    am_bsp_low_power_init();
+
     while (true)
     {
-        led1 = !led1;
-        thread_sleep_for(WAIT_TIME);
+        am_hal_gpio_output_toggle(LED1);
+        am_util_delay_ms(WAIT_TIME);
     }
 }
